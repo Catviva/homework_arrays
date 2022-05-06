89c52ea59154e83877dca2261280bbe2ce081bae
ekaterinasarapova <nkate05@mail.ru>
2022-05-06 20:56:22 +0300

    HW done

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,76 @@
+import java.util.Arrays;
+
 public class Main {
     public static void main(String[] args) {
         System.out.println("Hello world!");
 
+        // Exercise 1,2
+
+        int [] z = new int [3];
+        z[0] = 1;
+        z[1] = 2;
+        z[2] = 3;
+
+        int [] size = {1, 2, 3};
+        for (int i = 0; i < size.length; i++){
+            if (i != size.length - 1)
+            System.out.print(size[i] + ", ");
+            else
+                System.out.print(size[i]);
+        }
+
+        System.out.println();
+        double [] weight = {1.57, 7.654, 9.986};
+        for (int i = 0; i < weight.length; i++) {
+            System.out.print(weight[i] + ", ");
+        }
+
+        System.out.println();
+        int [] math = {2+3, 7*3, 5-3, 8/4};
+        for (int i = 0; i < math.length; i++) {
+            System.out.print(math[i] + ", ");
+        }
+
+
+        // Exercise 3
+
+        System.out.println();
+        int [] point = new int [] {1, 2, 3};
+        for (int i = point.length - 1; i >= 0; i--){
+            System.out.print(point[i] + ",");
+        }
+
+        System.out.println();
+        double [] amount = {1.57, 7.654, 9.986};
+        for (int i = amount.length - 1; i >= 0; i--) {
+            System.out.print(amount[i] + ",");
+        }
+
+        System.out.println();
+        int [] result = {2+3, 7*3, 5-3, 8/4};
+        for (int i = result.length - 1; i >= 0; i--) {
+            System.out.print(result[i] + ",");
+        }
+
+        // Exercise 4
+
+        System.out.println();
+        int [] number = {1, 2, 3};
+        for (int i = 0; 1 < number.length; i++){
+            if (number[i] % 2 != 0){
+
+                number[i]++;
+            }
+            if(i != number.length - 1)
+                System.out.print(number[i] + ",");
+            else
+                System.out.print(number[i]);
+
+
+
+            }
+
+
+        }
+
     }
-}
